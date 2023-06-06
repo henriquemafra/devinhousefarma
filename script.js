6b9da0dbe032d21145fdd14049c7f9b053a3da52
Henrique Mafra <henriquemafra89@gmail.com>
2023-06-06 13:16:00 -0300

    pequenas alterações no prototipo

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// Aumenta e Diminui quantidade de produtos
-
-function increaseQuantity(id) {
-    const input = document.getElementById(id);
-    let quantity = parseInt(input.value);
-    quantity++;
-    input.value = quantity;
-  }
-
-  function decreaseQuantity(id) {
-    const input = document.getElementById(id);
-    let quantity = parseInt(input.value);
-    if (quantity > 0) {
-      quantity--;
-    }
-    input.value = quantity;
-  }
-
-
 
   /* Swipper */
 
